b36e310b5c1b314bd53786d792b1ffad4e7dee3c
HernanWM <Waza.mango@gmail.com>
2022-05-05 23:14:43 +0000

    solucion de complifcto de mierda!

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,3 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+// import FormPage from "../component/formulario";
 import "../../styles/form.css";
