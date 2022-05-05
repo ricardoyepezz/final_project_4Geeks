26b2f18b3e43caf18bc923f3c8cb216a1ac4455a
HernanWM <48398407+HernanWM@users.noreply.github.com>
2022-05-05 19:18:30 -0400

    Revert "solucion de complifcto de mierda!"

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,4 +1,3 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-// import FormPage from "../component/formulario";
 import "../../styles/form.css";
