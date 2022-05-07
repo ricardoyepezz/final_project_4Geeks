88e7cbde464b510363c336e4b21a69786ffc1945
Carolig19 <ciribarrenguerra@gmail.com>
2022-05-07 16:51:42 +0000

    vistagenero

--- src/front/js/pages/animacion.js
+++ src/front/js/pages/animacion.js
@@ -3,9 +3,126 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { useEffect } from "react";
 export const Animacion = () => {
-    return (
-        <>
-<div>Animación</div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="row container-fluid col-12">
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade w-50"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2021/11/avatar-2520685.jpg?itok=-Znp5ZFb"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://i.ytimg.com/vi/_40CDeU_BwI/maxresdefault.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://i.ytimg.com/vi/gn1pz0rnNHs/maxresdefault.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+
+
+		<div className=" SideRight row col-6">
+        <div className="d-flex col-3">
+          <div class="card">
+		  <img src="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+		<div className="d-flex col-3">
+          <div class="card">
+		  <img src="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+		<div className="d-flex col-3">
+          <div class="card">
+		  <img src="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+		<div className="d-flex col-3">
+          <div class="card">
+		  <img src="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div></div>
+      </div>
+    </>
+  );
+};
