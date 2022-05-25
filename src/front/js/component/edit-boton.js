a130c5a1467202b251d6e18490e261d70926e4c1
Carolig19 <ciribarrenguerra@gmail.com>
2022-05-25 00:38:49 +0000

    editar perfil 1

---
+++ src/front/js/component/edit-boton.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Editar = () => {
+    const Click = () => {
+
+    }
+
+return(
+<button className="btn-warning" onClick={()=>Click}>Editar</button>
+)}
--- src/front/js/pages/configuracion.js
+++ src/front/js/pages/configuracion.js
@@ -2,17 +2,33 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { useEffect } from "react";
-import {Home} from "/workspace/final_project_4Geeks/src/front/js/pages/home.js";
-import {Menuperfil} from "/workspace/final_project_4Geeks/src/front/js/component/menuperfil.js"
-import {Sidebar} from "/workspace/final_project_4Geeks/src/front/js/component/sidebar.js"
+import { Home } from "/workspace/final_project_4Geeks/src/front/js/pages/home.js";
+import { Menuperfil } from "/workspace/final_project_4Geeks/src/front/js/component/menuperfil.js";
+import { Sidebar } from "/workspace/final_project_4Geeks/src/front/js/component/sidebar.js";
+import {Datos} from "../component/datos";
+import {Editar} from "../component/edit-boton";
 
 export const Configuracion = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <> <div className="cuerpo">
-    <div><Sidebar/></div>  
-    </div> 
+    <>
+      {" "}
+      <div className="perfil pt-12">
+        <div className="col-2">
+          {" "}
+          <Sidebar />
+        </div>
+        <div className="configuracion col-10">
+        <h1 className="text-warning">Configuración</h1>
+        <div className="container-fluid">
+        <div className="dato-config">
+        <div><Datos/><Editar/></div>
+
+      </div>
+        </div>
+        </div>
+      </div>
     </>
   );
-}
+};
